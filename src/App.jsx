3187d9a9da96002783101d57e906dc3e40eec799
engrpatel21 <engrpatel21@gmail.com>
2020-12-11 23:27:49 -0500

    header with appbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { Grid } from "@material-ui/core";
+import Header from "./Header/Header";
 
 function App() {
   return (
     <div className="App">
       <Grid container direction="column">
-        <Grid Item>Header</Grid>
+        <Grid Item>
+          <Header />
+        </Grid>
         <Grid item container>
           <Grid item xs={0} sm={2} />
           <Grid item xs={12} sm={8}>
--- src/theme.js
+++ src/theme.js
@@ -3,13 +3,7 @@ import purple from '@material-ui/core/colors/purple'
 import green from '@material-ui/core/colors/green'
 
 const theme = createMuiTheme({
-  palette: {
-    primary: purple,
-    secondary: green
-  },
-  status: {
-    danger: 'orange'
-  }
+
 })
 
 export default theme
